554b544cc14c35ace291eccbccd69178c7b0a9f2
Dominik Zeiger <dominik.zeiger@gmail.com>
2011-12-22 12:25:22 +0100

    added some yamaha specific constants

---
+++ Yamaha_Constants.ips.php
@@ -0,0 +1,11 @@
+<?
+	define("ya_NET_RADIO", "NET RADIO");
+	define("ya_AUDIO1", "AUDIO1");
+	define("ya_AUDIO2", "AUDIO2");
+	define("ya_AUDIO3", "AUDIO3");
+	define("ya_AUDIO4", "AUDIO4");
+	define("ya_AV1", "AV1");
+	define("ya_AV2", "AV2");
+	define("ya_AV3", "AV3");
+	define("ya_AV4", "AV4");
+?>
